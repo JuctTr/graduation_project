c99289da9d0db6997d6ea0c4873eb47cce05c49e
JuctTr <1301842163@qq.com>
2020-04-07 14:49:39 +0800

    no message

---
+++ server/config/config1.js
@@ -0,0 +1,28 @@
+/**
+ * @description 环境、数据库账号密码、端口等配置
+ */
+module.exports = {
+    // prod
+    environment:'dev',
+    database:{
+        dbName: 'graduation_project',
+        host: 'localhost',
+        port: 3306,
+        user: 'root',
+        password: 'root',
+    },
+    security:{
+        secretKey: "abcdefg", // 这个私有的密钥，在生成环境中是非常的复杂的和具有一定的规律的
+        expiredTime: 60*60*24*30 // 令牌的过期时间，项目上线记得把时间设置短一点
+    },
+    wx:{
+        appId: '',
+        appSecret: '',
+        loginUrl: 'https://api.weixin.qq.com/sns/jscode2session?appid=%s&secret=%s&js_code=%s&grant_type=authorization_code'
+    },
+    yushu:{
+        detailUrl: 'http://t.yushu.im/v2/book/id/%s',
+        keywordUrl: 'http://t.yushu.im/v2/book/search?q=%s&count=%s&start=%s&summary=%s'
+    },
+    host: 'http://localhost:3000/'
+}
